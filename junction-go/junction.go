8ce2bf35eca866b5d722d7e7f0ca5a83ce97d48d
Peter Travers <traverspw@gmail.com>
2025-04-23 14:24:25 -0400

    add serializing parameters

--- junction-go/junction.go
+++ junction-go/junction.go
@@ -2,19 +2,28 @@ package main
 
 import "C"
 import "unsafe"
+import "encoding/json"
+
 
 struct Client {
 	core interface{}
 }
 
 func (*Client) ResolveRoute(url string, method string, headers map[string]string, timeout int) ([]string, error) {
-	route := C.CString("some-destination")
+	url := C.CString(url)
 	defer C.free(unsafe.Pointer(route))
-
-	C.resolve_route(url, method, headers, timeout)
-	f err != nil {
+	method := C.CString(method)
+	defer C.free(unsafe.Pointer(method))
+	headersJson, err := json.Marshal(data)
+	if err != nil {
 		return nil, err
 	}
+	headers := C.CString(string(headersJson))
+	defer C.free(unsafe.Pointer(headers))
+	timeout := C.int(timeout)
+	defer C.free(unsafe.Pointer(timeout))
+	
+	C.resolve_route(url, method, headers, timeout, id)
 
 	return ips, nil
 }
